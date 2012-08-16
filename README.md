763274f5d40742c851a9d46a3981445968ad0d12
alepaez <alepaezseq@gmail.com>
2012-08-15 23:21:31 -0300

    Esqueleto do projeto com JSF e Hibernate

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 PI2
 ===
 http://www.jsftutorials.net/components/index.html
+http://docs.jboss.org/hibernate/orm/3.3/reference/en/html/tutorial.html#tutorial-firstapp
+http://netbeans.org/kb/docs/web/jsf20-intro_pt_BR.html
---
+++ src/java/pi2/curriculo/models/Curriculo.java
@@ -0,0 +1,20 @@
+package pi2.curriculo.models;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author alezonez
+ */
+public class Curriculo {
+    long id;
+    
+    
+    public Curriculo(){}
+        
+    
+    
+}
