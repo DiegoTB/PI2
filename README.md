07cb56f6b382cbd47786024404c53a6f0bf70e2a
alepaez <alepaezseq@gmail.com>
2012-08-16 05:31:38 -0300

    Mais um tutorial, agora um bom para seguir

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 PI2
 ===
 http://www.jsftutorials.net/components/index.html
+
+
 http://docs.jboss.org/hibernate/orm/3.3/reference/en/html/tutorial.html#tutorial-firstapp
+
+
 http://netbeans.org/kb/docs/web/jsf20-intro_pt_BR.html
+
+
+http://jamacedo.wordpress.com/2010/06/08/crud-jsf-2-0-hibernate-exemplo-gerenciando-livros-2/
--- src/java/pi2/curriculo/models/Curriculo.java
+++ src/java/pi2/curriculo/models/Curriculo.java
@@ -1,5 +1,10 @@
 package pi2.curriculo.models;
 
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -9,12 +14,37 @@ package pi2.curriculo.models;
  *
  * @author alezonez
  */
+
+@Entity
+@Table(name = "curriculos")
+
+
 public class Curriculo {
-    long id;
-    
     
     public Curriculo(){}
-        
+    
+    @Id
+    @Column(name = "id")
+    Integer id;
+    
+    @Column(name = "obs")
+    String obs;
+    
+    public Integer getId(){
+        return id;
+    }
+    
+    private void setId(Integer id){
+        this.id = id;
+    }
+    
+    public String getObs(){
+        return obs;
+    }
+    
+    public void setObs(String obs){
+        this.obs = obs;
+    }
     
     
 }
